f1f58f635067861f1ecc4eef40fc5394de1f22f5
KOUD BANGA Jancy Fridelin <jkoud@MacBook-Air-de-KOUD.local>
2025-02-02 19:54:41 +0100

    Correction du chemin de l'image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -57,7 +57,7 @@ const Photo = () => {
         {/* Image réduite et centrée dans le cercle */}
         <div className="w-[110px] h-[110px] xl:w-[180px] xl:h-[180px] relative z-10 overflow-hidden rounded-full flex justify-center items-center">
           <Image
-            src="/assets/kjb.jpg"
+            src="https://velvety-souffle-ba578f.netlify.app/assets/kjb.jpg"
             width={180}
             height={180}
             priority
